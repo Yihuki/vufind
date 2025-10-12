e8ce18a6d190171d5ce28bdbf5b00bd00e8ef116
Yihuki <wangbovbvb@gmail.com>
2025-10-12 20:28:58 +0800

    Add issn to primo backend to make browzine coverimage load work and reject empty url even fulltext label is true.

--- module/VuFind/src/VuFind/RecordDriver/Primo.php
+++ module/VuFind/src/VuFind/RecordDriver/Primo.php
@@ -229,10 +229,21 @@ class Primo extends DefaultRecord
      */
     public function getThumbnail($size = 'small')
     {
+        $params = ['size' => $size];
+
         if ($isbn = $this->getCleanISBN()) {
-            return ['size' => $size, 'isn' => $isbn];
+            $params['isn'] = $isbn;
         }
-        return ['size' => $size, 'contenttype' => 'JournalArticle'];
+
+        if ($issn = $this->getCleanISSN()) {
+            $params['issn'] = $issn; 
+        }
+
+        if (empty($params['isbn']) && empty($params['issn'])) {
+            $params['contenttype'] = 'JournalArticle';
+        }
+
+        return $params;
     }
 
     /**
@@ -253,7 +264,7 @@ class Primo extends DefaultRecord
     {
         $retVal = [];
 
-        if (isset($this->fields['url'])) {
+        if (isset($this->fields['url']) && !empty($this->fields['url'])) {
             $retVal[] = [];
             $retVal[0]['url'] = $this->fields['url'];
             if (isset($this->fields['fulltext'])) {
